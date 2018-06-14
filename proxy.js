738b22843c343150f626af352a907b9857011f5e
Andrew Liu <andrewliu@Andrews-MacBook-Pro-3.local>
2018-06-14 12:03:26 -0700

    commit before retstart pc

--- proxy.js
+++ proxy.js
@@ -4,17 +4,22 @@ const proxy = require('express-http-proxy');
 
 const app = express();
 
-app.use('/restaurant/:restaurant_id', express.static(__dirname + '/public'));
-
-app.get('/restaurant/:restaurant_id', function(req,res,next) {
+app.get('/', function(req,res,next) {
   console.log(req.url);
   next();
-})
+});
+
+app.use('/restaurant/:restaurant_id', express.static(__dirname + '/public'));
+
+app.get('/reservations/restaurant/:restaurant_id', function(req,res,next) {
+  console.log(req.url);
+  next();
+})  
 
 app.use('/reservations', proxy("http://localhost:3001"));
 
 app.listen(3000, function() {
-  console.log("listening to 3000")
+  console.log("listening to 3000");
 });
 
 module.exports = app;

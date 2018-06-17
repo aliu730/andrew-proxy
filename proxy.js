50bc94cf073b9f31d3a17fcb4c848ed35cbf2c5c
Andrew Liu <andrewliu@Andrews-MacBook-Pro-3.local>
2018-06-16 21:29:06 -0700

    proxy fixed

--- proxy.js
+++ proxy.js
@@ -5,18 +5,16 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.use('/', function(req,res,next) {
-  console.log("PROXY ROUTE", req.url);
-  console.log("proxy changed");
   next();
 });
 
 app.use('/restaurant/:restaurant_id', express.static(__dirname + '/public'));
 
 app.use('/reservations', proxy("http://cavareservations-env.sqh9cj65af.us-east-2.elasticbeanstalk.com/"));
-// app.use('/overviews', proxy("http://localhost:3003"));
-// app.use('/photos', proxy("http://localhost:3004"));
-// app.use('/menus', proxy('http://localhost:3005'));
-// app.use('/reviews', proxy('http://localhost:3002'));
+app.use('/overviews', proxy("http://cavatableoverviews-env.5sves92ky9.us-west-1.elasticbeanstalk.com/"));
+app.use('/photos', proxy("http://localhost:3004"));
+app.use('/menus', proxy('http://cavatablemenus-env.5sves92ky9.us-west-1.elasticbeanstalk.com/'));
+app.use('/reviews', proxy('http://CavatableFec-env.psexkp69kr.us-west-1.elasticbeanstalk.com'));
 app.listen(port, function() {
   console.log("listening to someproxy");
 });
